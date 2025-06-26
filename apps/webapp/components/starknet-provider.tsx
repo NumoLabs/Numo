8568dab1da35b87ee69c20e7c98e5df7d9d7f03b
DiegoB1911 <diego.barquero@hotmail.com>
2025-06-25 22:35:48 -0600

    feat: adjusting provider

--- apps/webapp/components/starknet-provider.tsx
+++ apps/webapp/components/starknet-provider.tsx
@@ -4,11 +4,12 @@ import React from "react";
 import { sepolia } from "@starknet-react/chains";
 import {
   StarknetConfig,
+  argent,
+  braavos,
   jsonRpcProvider,
+  useInjectedConnectors,
   voyager,
 } from "@starknet-react/core";
-import { InjectedConnector } from "starknetkit/injected";
-import { WebWalletConnector } from "starknetkit/webwallet";
 
 export function StarknetProvider({ children }: { children: React.ReactNode }) {
   function rpc() {
@@ -18,15 +19,15 @@ export function StarknetProvider({ children }: { children: React.ReactNode }) {
   }
   const provider = jsonRpcProvider({ rpc });
 
-  const connectors = [
-    new InjectedConnector({
-      options: { id: "argentX", name: "Argent X" },
-    }),
-    new InjectedConnector({
-      options: { id: "braavos", name: "Braavos" },
-    }),
-    new WebWalletConnector({ url: "https://web.argent.xyz" }),
-  ];
+  const { connectors } = useInjectedConnectors({
+    recommended: [
+      braavos(),
+      argent(),
+    ],
+    
+    includeRecommended: "onlyIfNoConnectors",
+    order: "random"
+  });
 
   return (
     <StarknetConfig
